98ae0d9e47e7bf1310bfa79594b357bf9bd3da48
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-10 14:34:25 +0100

    refactor: add search bar and navlist in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,21 +3,73 @@ import styled from "styled-components";
 const Header = (props) => {
   return (
     <Container>
-        <Content>
-            <Logo>
-            <a src="/home">
-                <img src="/images/home-logo.svg" alt="" />
-            </a>
-            </Logo>
-            <Search>
-            <div>
-                <input type="text" placeholder="Search" />
-            </div>
-            <SearchIcon>
-                <img src="/images/search-icon.svg" alt="" />
-            </SearchIcon>
-            </Search>
-        </Content>
+      <Content>
+        <Logo>
+          <a src="/home">
+            <img src="/images/home-logo.svg" alt="" />
+          </a>
+        </Logo>
+        <Search>
+          <div>
+            <input type="text" placeholder="Search" />
+          </div>
+          <SearchIcon>
+            <img src="/images/search-icon.svg" alt="" />
+          </SearchIcon>
+        </Search>
+        <Nav>
+          {" "}
+          <NavListWrap>
+            <NavList className="active">
+              <a src="/home">
+                <img src="/images/nav-home.svg" alt="" />
+                <span>Home</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a src="#">
+                <img src="/images/nav-network.svg" alt="" />
+                <span>My Network</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a src="#">
+                <img src="/images/nav-jobs.svg" alt="" />
+                <span>Jobs</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a src="#">
+                <img src="/images/nav-messaging.svg" alt="" />
+                <span>Messaging</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a src="#">
+                <img src="/images/nav-notifications.svg" alt="" />
+                <span>Notifications</span>
+              </a>
+            </NavList>
+            <User>
+              <a>
+                <img src="/images/user.svg" alt="" />
+                <span>Me</span>
+                <img src="/images/down-icon.svg" alt="" />
+              </a>
+              <SignOut>
+                <a>SignOut</a>
+              </SignOut>
+            </User>
+            <Work>
+              <a>
+                <img src="/images/nav-work.svg" alt="" />
+                <span>Products</span>
+                <img src="/images/down-icon.svg" alt="" />
+              </a>
+            </Work>
+          </NavListWrap>{" "}
+        </Nav>
+      </Content>
     </Container>
   );
 };
