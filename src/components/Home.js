652b911bee60d77bafa12913aa2614ccabba2741
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-05 13:41:38 +0100

    style: prepare to left , right and main side  place in Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,38 @@
 import styled from "styled-components";
 
 const Home = (props) => {
-
   return (
     <Container>
-        <Section>
-          <h5>
-            <a href="#"> Haring in a Harry ?</a>
-          </h5>
-          <p>
-            Find Talent pres in record time with upwork and keep business moving
-          </p>
-        </Section>
+      <Section>
+        <h5>
+          <a href="#"> Haring in a Harry ?</a>
+        </h5>
+        <p>
+          Find Talent pres in record time with upwork and keep business moving
+        </p>
+      </Section>
+      <Layout>
+        <div>Left Side</div>
+        <div>Main</div>
+        <div>Right Side</div>
+      </Layout>
     </Container>
   );
-
 };
-export default Home;
 const Container = styled.div`
-  padding-top: 100px;
+  padding-top: 52px;
   max-width: 100%;
 `;
+
 const Content = styled.div`
   max-width: 1128px;
-
   margin-left: auto;
   margin-right: auto;
 `;
-const Section = styled.div`
+
+const Section = styled.section`
   min-height: 50px;
+  padding: 16px 0;
   box-sizing: content-box;
   text-align: center;
   text-decoration: underline;
@@ -51,3 +55,20 @@ const Section = styled.div`
     padding: 0 5px;
   }
 `;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+export default Home;
