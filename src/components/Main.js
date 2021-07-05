9610ccc2b2f2c345c99da751af1257ff979c4806
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-05 14:25:10 +0100

    refactor: add the images folder

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import styled from "styled-components";
 const Main = () => {
     return (
-        <div>
+        <Container>
             Main
-        </div>
+        </Container>
     )
 }
+const Container = styled.div`
+  grid-area: main;
+`;
 
 export default Main
